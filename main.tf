09d9da279b8f414af66444013dfef4c207608ad9
nsuazo <nicholas.suazo@gmail.com>
2023-07-27 14:21:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
 provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
     region = "${var.region}"
 }
 
 module "s3" {
-    source = "<path-to-S3-folder>"
+    source = "modules/"
     #bucket name should be unique
     bucket_name = "tts-jenkins-test-bucket"       
 }
